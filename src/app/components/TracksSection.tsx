64ed5af98cb123ff10c4ae57fce5d245c2c9f1a9
Scriptscrypt <tirumaladasa@gmail.com>
2024-08-26 18:04:54 +0530

    Feat: Dialog - ShadCN

--- src/app/components/TracksSection.tsx
+++ src/app/components/TracksSection.tsx
@@ -3,21 +3,48 @@ import React from "react";
 import TracksBottomBG from "@/assets/svgs/bgs/TracksBottomBG.svg";
 import TracksTopBG from "@/assets/svgs/bgs/TracksTopBG.svg";
 import { TrackCardBg } from "@/assets/bgs/TrackCardBg";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 const TrackContainer = () => {
   return (
-    <div className="relative">
-      <div className="absolute p-12 flex flex-col gap-1">
-        <p className="text-[42px] leading-none">Track name</p>
-        <p className="text-[26px] leading-none">Track Description</p>
+    <>
+      <div className="relative">
+        <div className="absolute p-12 flex flex-col gap-1">
+          <p className="text-[42px] leading-none">Track name</p>
+          <p className="text-[26px] leading-none">Track Description</p>
+        </div>
+        <TrackCardBg />
       </div>
-      <TrackCardBg />
-    </div>
+
+      <Dialog>
+        <DialogTrigger>Open</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
 const TracksSection = () => {
   return (
-    <div className="min-h-screen relative flex flex-row justify-between items-center bg-[#1C71FF]">
+    <div
+      id="tracks"
+      className="min-h-screen relative flex flex-row justify-between items-center bg-[#1C71FF]"
+    >
       <div className="w-full flex flex-col items-center justify-center">
         <Image
           className="w-full h-full"

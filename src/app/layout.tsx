3bbc319935be57a5e99a40c20580c5d76c9c0a4a
aryan <agarwalaryan139@gmail.com>
2024-08-26 21:50:20 +0530

    fix: og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,42 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'Blinkathon',
   description: 'Blinks Hackathon',
+  openGraph: {
+    title: 'Blinkathon',
+    description: 'Blinks Hackathon',
+    url: 'https://www.blinkathon.fun',
+    siteName: 'Blinkathon',
+    images: [
+      {
+        url: 'https://www.blinkathon.fun/banner.jpeg',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Blinkathon',
+    description: 'Blinks Hackathon',
+    site: '@thesendcoin',
+    images: ['https://www.blinkathon.fun/banner.jpeg'],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 const myFont = localFont({

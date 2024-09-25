942d7d92dff34119ba8461f59b6350a66e14464b
Scriptscrypt <tirumaladasa@gmail.com>
2024-09-25 17:50:22 +0530

    Feat: Remove useless CTAs

--- src/app/components/results/ResultsHero.tsx
+++ src/app/components/results/ResultsHero.tsx
@@ -10,9 +10,9 @@ const ResultsHero = () => {
         <div className="flex flex-col items-center mt-32">
           <p className="text-[104px] text-[#1C71FF] z-50">Flex your Blinks</p>
           <p className="text-[40px] text-[#1C71FF] z-50">
-            Submit your Project here!
+           All your Blinkathon Submissions in one place
           </p>
-          <div className="cursor-pointer">
+          {/* <div className="cursor-pointer">
             <Image
               className="-z-40"
               src={blueSvgCTA}
@@ -23,7 +23,7 @@ const ResultsHero = () => {
             <div className="-mt-12 ml-10 text-[#fff] text-[24px] z-50">
               Submit Blinks
             </div>
-          </div>
+          </div> */}
         </div>
         <Image
           className="w-full h-full z-0 -mt-72"
